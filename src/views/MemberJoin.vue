b9242e53dcccb9cfbb2e5a57cd6494645298ac06
romano1994 <fhaksh0369@gmail.com>
2024-09-16 21:32:36 +0900

    회원가입 페이지 및 유효성 검사

--- src/views/MemberJoin.vue
+++ src/views/MemberJoin.vue
@@ -1,13 +1,188 @@
 <template lang="">
-    <div>
-        회원 가입 페이지입니다.
+    <div class="join-window">
+        <div class="join-text">
+            회원가입
+        </div>
+        <div class="container">
+            <div class="text-div">
+                이메일
+            </div>
+            <div class="input-div">
+                <input class="input-box" v-model="inputEmail">
+                <div class="valid-div" v-show="valid.email">
+                    이메일을 확인해주세요.
+                </div>
+            </div>
+            <div class="text-div">
+                비밀번호
+            </div>
+            <div class="input-div">
+                <input type="password" class="input-box" v-model="inputPwd">
+                <div class="valid-div" v-show="valid.pwd">
+                    영문, 숫자, 특수문자를 조합하여 입력해주세요.
+                </div>
+            </div>
+            <div class="text-div">
+                확인 비밀번호
+            </div>
+            <div class="input-div">
+                <input type="password" class="input-box" v-model="inputPwdChk">
+                <div class="valid-div" v-show="valid.pwdChk">
+                    입력된 비밀번호가 다릅니다.
+                </div>
+            </div>
+            <div class="text-div">
+                이름
+            </div>
+            <div class="input-div">
+                <input class="input-box" v-model="inputNm">
+            </div>
+            <div></div>
+            <div class="button-div">
+                <button class="button-box" @click="fnConfirmJoin">
+                    확인
+                </button>
+                <button class="button-box">
+                    취소
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            valid: {
+                email:  false,
+                pwd:    false,
+                pwdChk: false,
+            },
+            inputEmail:  "",
+            inputPwd:    "",
+            inputPwdChk: "",
+            inputNm:     "",
+        }
+    },
+    watch: {
+        'inputEmail': function() {
+            this.checkEamil();
+        },
+        'inputPwd': function() {
+            this.checkPwd();
+        },
+        'inputPwdChk': function() {
+            this.checkPwdChk();
+        }
+    },
+
+    methods: {
+        //이메일 형식 체크
+        checkEamil() {
+            //xxx@xxx.xxx 형식
+            const validateEmail = /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-Za-z0-9\\-]+/;
+
+            if(!validateEmail.test(this.inputEmail) || !this.inputEmail) {
+                this.valid.email = true;
+                return;
+
+            } else {
+                this.valid.email = false;
+                return;
+            }
+        },
+
+        //비밀범호 형식 체크
+        checkPwd() {
+            //숫자, 영어 대 소문자, 특수문자 포함 8자리 이상
+            const validatePwd = /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,16}$/;
+
+
+            if(!validatePwd.test(this.inputPwd) || !this.inputPwd) {
+                this.valid.pwd = true;
+                return;
+
+            } else {
+                this.valid.pwd = false;
+                return;
+            }
+        },
+
+        //비밀번호 확인 체크
+        checkPwdChk() {
+            if(this.inputPwd != this.inputPwdChk) {
+                this.valid.pwdChk = true;
+                return;
+            } else {
+                this.valid.pwdChk = false;
+                return;
+            }
+        },
+
+        //회원 가입 확인
+        fnConfirmJoin() {
+            
+        },
+    }
 }
 </script>
-<style lang="">
-    
+<style>
+    .join-window{
+        height: 40rem;
+        align-content: center;
+        justify-content: center;
+    }
+    .join-text{
+        text-align: center;
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+    .container {
+        display: grid;
+        height: 500px;
+        grid-template-columns: repeat(5, 1fr);
+        row-gap: 10px;
+    }
+    .text-div {
+        text-align: end;
+        font-size: 1.2rem;
+        grid-column-start: 2;
+        align-content: center;
+        padding-right: 30px;
+    }
+    .input-div{
+        grid-column-start: 3;
+        grid-column-end: 5;
+        margin-left: 2rem;
+        align-content: center;
+    }
+    .input-box{
+        width: 18rem;
+        height: 1.5rem;
+    }
+    .button-div{
+        grid-column-start: 2;
+        grid-column-end: 5;
+        display: flex;
+        justify-content: space-evenly;
+        padding-right: 1rem;
+    }
+    .button-box{
+        width: 5rem;
+        height: 2rem;
+        background-color: var(--color1);
+    }
+    .valid-div{
+        position: absolute;
+        padding-top: 8px;
+    }
+    .button-box:hover{
+        transform: scale(1.14);
+        background-color: white;
+        box-shadow: 0px 15px 20px rgba(27, 39, 63, 0.4);
+    }
+    input[type="password"] {
+        font: small-caption;
+        font-size: 20px;
+    }
 </style>
