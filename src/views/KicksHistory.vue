5deb80aa9d6ca86e269c8f1dd1d7543785113d63
Heon Hong <ghdgjs3027@gmail.com>
2024-12-05 00:56:56 +0900

    feat. 카드 움직임 및 반짝거림 완성(css 작업 추가 필요)

--- src/views/KicksHistory.vue
+++ src/views/KicksHistory.vue
@@ -1,23 +1,52 @@
 <template lang="">
     <div class="page-container grid-column-3">
-        <div class="card-container" v-for="(item,index) in cards" :key="index">
-            <div class="outer card" >
+        <!-- <div class="card-container" v-for="(item,index) in cards" :key="index">
+            <div class="outer card" :ref="`jordan${index}`" @mousemove="e=>moveCard(e,index,true)"
+                @mouseleave="e=>moveCard(e,index,false)">
+                <div class="overlay card"></div>
+
                 <div class="outer-layer p-2">
                     <div class="inner card p-3" >
-                        <div class="card-title border border-secondary">
+                        <div class="card-title ">
                             <span class="badge bg-warning card-badge">
                                 Best
                             </span>
                                 {{item.commCdDtlNm}}
-                    </div>
+                        </div>
                         <div class="card-background"></div>
-                        <div class="card-text">
-                            {{item.jrdHstrySmmryCntnt}}
+                        <div class="card-container">
+                            <div class="card-text">
+                                {{item.jrdHstrySmmryCntnt}}
+                            </div>
                         </div>
                     </div>
                     <img class="card-img-top" :src="getImgUrl(item.imgPath)" alt="jrd-img">
                 </div>
-        </div>
+            </div>
+        </div> -->
+
+        <div class="card-container" >
+            <div class="outer card" :ref="`jordan1`" @mousemove="e=>moveCard(e,1,true)"
+                @mouseleave="e=>moveCard(e,1,false)">
+                <div class="overlay" ref="overlay1"></div>
+                <div class="outer-layer p-2">
+                    <div class="inner card p-3" >
+                        <div class="card-title ">
+                            <span class="badge bg-warning card-badge">
+                                Best
+                            </span>
+                                Jordan 1
+                        </div>
+                        <div class="card-background"></div>
+                        <div class="card-container">
+                            <div class="card-text">
+                                조던 1은 .....
+                            </div>
+                        </div>
+                    </div>
+                    <img class="card-img-top" src="@/assets/jordan1.webp" alt="jrd-img">
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -48,6 +77,41 @@ export default {
         },
         fail(err) {
             console.log(err);
+        },
+        moveCard(e, idx, flag) {
+            // const parent = this.$refs[`jordan${idx}`][0];
+            // console.log("parent",parent);
+            // console.log("e.target", e.target);
+
+            // if(e.target!=parent){
+            //     return;
+            // }
+
+            if (!flag) {
+                // this.$refs[`jordan${idx}`][0].style.transform = `rotateY(0deg) rotateX(0deg)`;
+                this.$refs[`jordan1`].style.transform = `rotateY(0deg) rotateX(0deg)`;
+                // this.$refs[`jordan1`].style.transition = `all 0.1s`;
+
+                // this.$refs[`jordan${idx}`][0].style.transition=`all ease 0.5s`;
+                this.$refs['overlay1'].style='background-position : none; filter:opacity(0)';
+
+                return;
+            }
+
+            let offsetX = e.offsetX;
+            let offsetY = e.offsetY;
+
+            let width = e.target.getBoundingClientRect().width;
+            let height = e.target.getBoundingClientRect().height;
+
+            let yDegree = (offsetX - width / 2) / width * 60;
+            let xDegree = (offsetY - height / 2) / height * 60;
+            console.log("yDegree", yDegree);
+            console.log("yDegree", xDegree);
+
+            // this.$refs[`jordan${idx}`][0].style.transform = `perspective(${height * 10}px) rotateY(${yDegree}deg) rotateX(${xDegree}deg)`;
+            this.$refs[`jordan1`].style.transform = `perspective(${height * 10}px) rotateY(${yDegree}deg) rotateX(${xDegree}deg)`;
+            this.$refs['overlay1'].style=`background-position : ${yDegree}%`;
 
         },
     },
@@ -57,17 +121,43 @@ export default {
 .card-container {
     display: flex;
     justify-content: center;
-    margin: 1rem;
+    margin: 1.5rem;
     border: none;
+}
+
+.card {
+    transition: all 0.5s;
+    width: 23rem;
+    height: 32rem;
+}
+
+.overlay {
+    position: absolute;
+    width: 23rem;
+    height: 32rem;
+    /* radial-gradient 로 변경 */
+    background: linear-gradient(105deg,
+            transparent 40%,
+            rgba(255, 219, 122, 0.8) 45%,
+            rgba(132, 50, 255, 0.8) 45% ,
+            transparent 54%);
+    filter: brightness(1.2) opacity(0.8);
+    mix-blend-mode: color-dodge;
+    background-size:150% 150%;
+    background-position: 100%;
 }
 
 .outer {
     background-image: url(@/assets/wave_back.png);
     background-size: cover;
     align-items: center;
-    width: 23rem;
-    height: 32rem;
+
 }
+
+/* .outer:hover{
+    transform:rotateY(20deg);
+
+} */
 
 .outer-layer {
     display: flex;
@@ -92,8 +182,9 @@ export default {
     font-weight: bold;
     font-size: 1.3rem;
     background-color: var(--color1);
-    width: calc(100%-10px);
-
+    /* width: calc(100% - 1rem); */
+    width: 19rem;
+    padding: 2px 2rem;
 }
 
 .card-badge {
@@ -112,17 +203,28 @@ export default {
     border: 1mm ridge rgba(211, 220, 50, .6);
 }
 
+.card-container {
+    /* padding: 1rem 1rem 0 0; */
+    width: inherit;
+}
+
 .card-text {
     max-height: 7rem;
-    overflow: auto;
+    overflow-y: scroll;
+    padding: 0.5rem;
+    -ms-overflow-style: none;
+}
+
+.card-text::-webkit-scrollbar {
+    display: none;
 }
 
 .card-img-top {
     position: absolute;
-    z-index: 99;
     width: 26rem;
-    top: -19%;
+    top: -13%;
     left: 46%;
     transform: translateX(-50%) rotate(-25deg);
+    pointer-events: none;
 }
 </style>
